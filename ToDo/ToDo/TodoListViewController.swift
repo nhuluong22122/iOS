e12175b416f100a181975e24ca643d6bade4da96
Nhu Luong <nhuluong2212@gmail.com>
2018-01-04 12:42:32 -0800

    To Do List App without data persistence

---
+++ ToDo/ToDo/TodoListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Nhu Luong on 1/4/18.
+//  Copyright © 2018 Nhu Luong. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Grocery", "SWE", "AWS Study"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else { tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    
+
+}
+
